48dffd93c44ae3973c3cf68d7417a05f843b0336
Tom Hüttmann <Tom.Huettmann@otto.de>
2026-06-30 17:50:12 +0200

    update cac to 1.3.0

--- Formula/cac.rb
+++ Formula/cac.rb
@@ -1,11 +1,11 @@
 class Cac < Formula
   desc "Co-Author Committer - A fast terminal UI tool to amend your latest git commit with co-authors"
   homepage "https://github.com/tomhuettmann/cac"
-  version "1.2.0"
+  version "1.3.0"
   license "MIT"
 
   url "https://github.com/tomhuettmann/cac/releases/download/v#{version}/cac-#{version}-macos-arm64.tar.gz"
-  sha256 "d7dd7e8b320ebd5477496a416862826f4c7700fc5729ddc4235d5fde636fc328"
+  sha256 "8b1473968ed93388add00b76ed4734bad473928d9772ef9eaf044da88003e743"
 
   def install
     bin.install "cac"
